ea87769d3b5623d103e839f6dc959e61a95e6df5
matoommatom <matoom.14514@gmail.com>
2019-12-11 16:15:56 +0700

    add parti detail page and button

--- Parti/src/app/parti-detail/parti-detail.page.ts
+++ Parti/src/app/parti-detail/parti-detail.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { partiGroup, partiUser, parties } from 'src/models/user.model';
+import { PartiService } from 'src/providers/parti.service';
+import { AngularFirestore } from 'angularfire2/firestore';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-parti-detail',
@@ -6,10 +11,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parti-detail.page.scss'],
 })
 export class PartiDetailPage implements OnInit {
+  group:partiGroup;
+  user:partiUser;
+  partiID:string = this.partiService.partiIDtoshow;
+  partiToView:parties;
+  members:Array<any>;
+  pending:Array<any>;
 
-  constructor() { }
-
+  constructor(
+    private partiService: PartiService,
+    private afs: AngularFirestore,
+  ) { 
+    console.log(this.partiID);
+    this.afs.collection("parties").doc<parties>(this.partiID)
+    .valueChanges()
+    .subscribe(party=>{
+      this.partiToView = party;
+      console.log(this.partiToView);
+      this.members= party.members;
+      console.log(this.members);
+      this.pending = party.pendingMembers;
+      console.log(this.pending);
+    })
+  }
+  
   ngOnInit() {
   }
-
+  convertTimeStamp(timestamp:any){
+    const time = Number(timestamp.seconds);
+    return moment.unix(time).format('DD/MM/YYYY');
+  }
 }
